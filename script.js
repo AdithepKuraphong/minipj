2e2c5fb877b2cb290f1cbc1bd01f6131f630335c
AdithepKuraphong <99580694+AdithepKuraphong@users.noreply.github.com>
2023-09-05 00:18:30 +0700

    fix script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const popCat = document.getElementById('banana');
+const clickOn = document.getElementById('banana');
 const scoreElement = document.getElementById('score');
 let score = 0;
 
-popCat.addEventListener('click', () => 
+clickOn.addEventListener('click', () => 
 {
     score++;
     scoreElement.textContent = score;
-    popCat.style.transform = 'scale(1.1)';
-    setTimeout(() => {
-        popCat.style.transform = 'scale(1)';
+    clickOn.style.transform = 'scale(1.5)';
+    setTimeout(() => 
+    {
+        clickOn.style.transform = 'scale(1)';
     }, 100);
 });
